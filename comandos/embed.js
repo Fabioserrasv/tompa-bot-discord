2fa04acc978fb66525380da62fe487e749d7392f
Fabio Serra <fabioserra.sv@gmail.com>
2022-03-15 20:12:47 -0300

    Novo comando de embed de twitter

---
+++ comandos/embed.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    function emoji (id){
+        return client.emojis.cache.get(id).toString();
+    }
+    
+    //const hornyJail = client.channels.cache.find(channel => channel.id === '755098963098599524')
+
+    message.channel.messages.fetch({ limit: 5 }).then(messages => {
+        let lastMessage = messages.first();
+        
+        messages.every(mensagem =>{
+          if(mensagem.content.includes('https://twitter.com')){
+            let novoLink
+
+            if(args[0] == 'n' || args[0] == 'nitter' || args[0] == 'ni'){
+              novoLink = mensagem.content.replace("https://twitter.com", "https://nitter.silkky.cloud")
+            }else{
+              novoLink = mensagem.content.replace("https://twitter.com", "https://fxtwitter.com")
+            }
+
+            message.channel.send(novoLink)
+            return false
+          }
+          return true
+        })
+      })
+      .catch(console.error);
+}
